920127fef7a148de0742148780b982d3a5fd7af6
krisanpaulino <36087959+krisanpaulino@users.noreply.github.com>
2025-06-19 12:44:54 +0800

    Update layout_admin.php

--- app/Views/layout_admin.php
+++ app/Views/layout_admin.php
@@ -168,11 +168,11 @@
                             </div>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="javascript:;"><i class="bx bx-user"></i><span>Profile</span></a>
-                            </li>
-                            <li>
+                            <!-- <li><a class="dropdown-item" href="javascript:;"><i class="bx bx-user"></i><span>Profile</span></a>
+                            </li> -->
+                            <!-- <li>
                                 <div class="dropdown-divider mb-0"></div>
-                            </li>
+                            </li> -->
                             <li>
                                 <form action="<?= base_url('auth/logout') ?>" method="post">
                                     <button class="dropdown-item" type="submit"><i class='bx bx-log-out-circle'></i><span>Logout</span></button>
